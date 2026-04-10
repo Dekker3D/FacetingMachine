4780056809ffe6dddfed0790de223ce1cdd1bc83
Nova Agent <nova@dauntlessrooster.com>
2026-04-10 14:01:16 +0200

    User refinements: simplified fillets/edges (0.1mm), fixed val() chains/geometry errors; CQ-Editor stable

--- mast_design.py
+++ mast_design.py
@@ -30,138 +30,130 @@ BEARING_OD = 15.0  # For hinge system
 BEARING_ID = 8.0   # Inner diameter
 BEARING_WIDTH = 7.0
 
-def make_bearing_mount():
-    '''Simple bearing mount for leadscrew ends'''
-    result = (cq.Workplane("XY")
-              .cylinder(20, LEADSCREW_DIA/2 + 5)
-              .faces(">Z").workplane()
-              .circle(BEARING_OD/2).cutBlind(-BEARING_WIDTH)
-              .faces(">Z").workplane()
-              .circle(LEADSCREW_DIA/2 + 0.1).cutBlind(-20))
-    
-    # Mounting tab for t-slot extrusion
-    tab = (cq.Workplane("XY")
-           .box(15, 8, 8)
-           .translate((0, -T_SLOT_SIZE/2 - 4, 0)))
-    return result.union(tab)
+# New realistic dimensions
+RAIL_LENGTH = 250.0
+LEADSCREW_LENGTH = 400.0
+T_SLOT_LENGTH = 450.0
 
-def make_carriage_adapter():
-    '''Adapter for MGN9H carriage to connect to leadscrew nut'''
-    result = cq.Workplane("XY").box(50, 30, 20)
-    
-    # Carriage pocket from bottom
-    result = result.faces("<Z").workplane().rect(CARRIAGE_WIDTH + 4, CARRIAGE_LENGTH + 4).cutBlind(20)
-    
-    # Nut pocket from top
-    result = result.faces(">Z").workplane().circle(NUT_DIA/2 + 2).cutBlind(-20)
-    
-    # Nut mounting holes (batch cut)
-    hole_positions = [(NUT_HOLE_RADIUS * math.cos(math.radians(a)), NUT_HOLE_RADIUS * math.sin(math.radians(a))) for a in [0, 90, 180, 270]]
-    result = result.faces(">Z").workplane().pushPoints(hole_positions).circle(NUT_HOLE_DIA/2).cutBlind(-20)
-    
-    return result
+def make_t_slot_extrusion(length=T_SLOT_LENGTH):
+    '''Accurate 20x20 T-slot profile with slots on all sides - fixed co-planar'''
+    shell = cq.Workplane("XZ").box(T_SLOT_SIZE, length, T_SLOT_SIZE)
+    # Top slot
+    shell = shell.faces(">Z").workplane().rect(10, 6).cutBlind(-1.5)
+    # Bottom slot
+    shell = shell.faces("<Z").workplane().rect(10, 6).cutBlind(-1.5)
+    # Right side slot
+    shell = shell.faces(">Y").workplane().rect(6, 10).cutBlind(-1.5)
+    # Left side slot
+    shell = shell.faces("<Y").workplane().rect(6, 10).cutBlind(-1.5)
+    profile = shell.edges("|Z or |X or |Y").fillet(0.1)
+    return profile
 
-def make_hinge():
-    '''Hinge system for quill assembly with 15mm OD bearings'''
-    result = (cq.Workplane("XY")
-              .cylinder(BEARING_WIDTH + 10, BEARING_OD/2 + 2)
-              .faces(">Z").workplane()
-              .circle(BEARING_OD/2).cutBlind(-BEARING_WIDTH)
-              .faces(">Z").workplane()
-              .circle(BEARING_ID/2).cutBlind(-BEARING_WIDTH - 10))
-    
-    # Mounting face
-    mount = (cq.Workplane("XY")
-             .box(30, 20, 5)
-             .translate((0, 0, -BEARING_WIDTH - 2.5)))
-    return result.union(mount)
+def make_mgn9_rail(length=RAIL_LENGTH):
+    '''MGN9 rail profile with mounting holes'''
+    rail = cq.Workplane("XY").box(RAIL_WIDTH, length, RAIL_HEIGHT)
+    rail = rail.edges(">Z").fillet(0.8)
+    # Mounting holes every 30mm
+    num_holes = int(length / 30) + 1
+    hole_positions = [(RAIL_WIDTH/2 + i*30, 0) for i in range(num_holes)]
+    rail = cq.Workplane("XY").add(rail).faces(">Z").workplane().pushPoints(hole_positions).circle(3.2/2).cutThruAll()
+    return rail.val()
+
+def make_mgn9_carriage():
+    '''MGN9H carriage'''
+    carriage = cq.Workplane("XY").box(CARRIAGE_WIDTH, CARRIAGE_LENGTH, CARRIAGE_HEIGHT).translate((0, 0, RAIL_HEIGHT/2 + CARRIAGE_HEIGHT/2 - CARRIAGE_CLEARANCE))
+    carriage = carriage.edges("|Z").fillet(1.5)
+    return carriage
+
+def make_pillow_block():
+    '''Pillow block bearing mount for T8 leadscrew (608ZZ recess)'''
+    block = cq.Workplane("XY").box(28, 32, 16)
+    # Shaft hole
+    block = block.faces("<Z").workplane().circle(LEADSCREW_DIA/2 + 0.2).cutThruAll()
+    # Bearing recess
+    block = block.faces(">Z").workplane(offset=8).circle(BEARING_OD/2).cutBlind(-14)
+    # Mounting tabs with M4 holes
+    tabs = (cq.Workplane("XY").box(22, 12, 6).translate((0, -16, -5))
+            .faces(">Z").workplane().pushPoints([(-9, 0), (9, 0)]).circle(4.2/2).cutThruAll())
+    block = block.union(tabs)
+    block = block.edges().fillet(0.1)
+    return block
+
+def make_t8_shaft(length=LEADSCREW_LENGTH):
+    '''T8 leadscrew shaft visualization'''
+    shaft = cq.Workplane("XY").cylinder(length, LEADSCREW_DIA/2).translate((0, -15, 225))
+    return shaft.val()
+
+def make_t8_nut():
+    '''T8 leadscrew nut with flange and mounting holes'''
+    nut_body = cq.Workplane("XY").cylinder(15, 11).translate((0, 0, 1.75))
+    flange = cq.Workplane("XY").circle(11).extrude(NUT_THICKNESS).translate((0, 0, 3.5))
+    angles = [0, 90, 180, 270]
+    hole_positions = [(NUT_HOLE_RADIUS * math.cos(math.radians(a)), NUT_HOLE_RADIUS * math.sin(math.radians(a))) for a in angles]
+    flange = flange.faces(">Z").workplane().pushPoints(hole_positions).circle(NUT_HOLE_DIA/2).cutThruAll()
+    nut = nut_body.union(flange)
+    nut = nut.edges().fillet(0.8)
+    return nut
+
+def make_quill_hinge():
+    '''Improved quill hinge with bearing recess'''
+    hinge = cq.Workplane("XY").box(35, 22, 12)
+    hinge = hinge.faces("<Z").workplane().circle(BEARING_OD/2).cutThruAll()
+    hinge = hinge.faces(">Z").workplane(offset=10).circle(BEARING_ID/2 + 0.1).cutBlind(-12)
+    mount_tab = cq.Workplane("XY").box(30, 15, 5).translate((0, 11, 0))
+    hinge = hinge.union(mount_tab)
+    hinge = hinge.edges().fillet(1.2)
+    return hinge
 
 def make_linear_rail_mount():
-    '''Mount for MGN9H rail attached to t-slot extrusion'''
+    '''Updated rail mount with fillets'''
     result = cq.Workplane("XY").box(60, 30, 8)
-    
-    # Rail pocket from bottom
     result = result.faces("<Z").workplane(offset=-4).rect(RAIL_WIDTH + 2, RAIL_HEIGHT + 2).cutBlind(-8)
-    
-    # Mounting holes (batch cut: LR ±7.5 y=0; UD x=0 ±8)
     hole_positions = [(-7.5, 0), (7.5, 0), (0, -8), (0, 8)]
     result = result.faces(">Z").workplane().pushPoints(hole_positions).circle(1.65).cutBlind(-8)
-    
-    return result
-
-def make_t_slot_piece():
-    '''T-slot extrusion piece for visualization'''
-    # Main body (length along Y)
-    main = (cq.Workplane("XZ")
-            .box(T_SLOT_SIZE, 400, T_SLOT_SIZE, centered=(True, False, True)))  # Longer for assembly
-    
-    # T-slots
-    result = main.faces(">Z").workplane().rect(10, 6).cutBlind(-8)
-    result = result.faces("<Z").workplane().rect(10, 6).cutBlind(-8)
+    # Not enough space, error on fillet(1.0)
+    result = result.edges().fillet(0.1)
     return result
 
 def make_assembly():
-    '''Idiomatic CadQuery Assembly'''
+    '''Improved realistic assembly'''
     assembly = cq.Assembly()
-    
-    # Main t-slot extrusion (base, long)
-    extrusion = make_t_slot_piece()
-    assembly.add(extrusion, name='extrusion', loc=Location((0, 0, 0)))
-    
-    # Linear rail mount on extrusion
-    rail_mount = make_linear_rail_mount()
-    assembly.add(rail_mount, name='rail_mount', loc=Location((0, 0, 0)))
-    
-    # Leadscrew bearing mounts (behind rail)
-    top_bearing = make_bearing_mount()
-    assembly.add(top_bearing, name='top_bearing', loc=Location((0, -30, 200)))
-    
-    bottom_bearing = make_bearing_mount()
-    assembly.add(bottom_bearing, name='bottom_bearing', loc=Location((0, -30, 0)))
-    
-    # Carriage adapter (mid-rail height)
-    rail_mid = RAIL_HEIGHT/2 + CARRIAGE_HEIGHT/2
-    carriage_adapter = make_carriage_adapter()
-    assembly.add(carriage_adapter, name='carriage', loc=Location((0, rail_mid, 100)))
-    
-    # Quill hinge above carriage
-    hinge = make_hinge()
-    assembly.add(hinge, name='hinge', loc=Location((0, rail_mid + 20, 100)))
-    
+    assembly.add(make_t_slot_extrusion(), name='extrusion', loc=Location((0, 0, 0)))
+    assembly.add(make_linear_rail_mount(), name='rail_mount', loc=Location((0, 0, 0)))
+    assembly.add(make_mgn9_rail(), name='rail', loc=Location((0, 1, 6)))
+    assembly.add(make_pillow_block(), name='bottom_bearing', loc=Location((0, -28, 5)))
+    assembly.add(make_pillow_block(), name='top_bearing', loc=Location((0, -28, 220)))
+    assembly.add(make_mgn9_carriage(), name='carriage', loc=Location((0, 12, 110)))
+    assembly.add(make_t8_shaft(), name='leadscrew', loc=Location((0, 0, 0)))
+    assembly.add(make_t8_nut(), name='nut', loc=Location((0, -15, 110)))
+    assembly.add(make_quill_hinge(), name='hinge', loc=Location((0, 25, 110)))
     return assembly
 
 def export_all_parts():
-    '''Export all individual parts + assembly'''
-    print("Exporting parts...")
-    
+    '''Export all parts and assembly'''
+    print("Exporting improved parts...")
     try:
-        # Individual parts (translate for separation)
-        cq.exporters.export(make_bearing_mount().translate((0, 0, 100)), 'top_bearing_mount.stl')
-        print("Top bearing mount exported")
-        
-        cq.exporters.export(make_bearing_mount(), 'bottom_bearing_mount.stl')
-        print("Bottom bearing mount exported")
-        
+        cq.exporters.export(make_t_slot_extrusion().translate((100, 0, 0)), 't_slot_extrusion.stl')
+        print("T-slot extrusion exported")
+        cq.exporters.export(make_mgn9_rail(), 'mgn9_rail.stl')
+        print("MGN9 rail exported")
+        cq.exporters.export(make_mgn9_carriage(), 'mgn9_carriage.stl')
+        print("MGN9 carriage exported")
+        cq.exporters.export(make_pillow_block(), 'pillow_block.stl')
+        print("Pillow block exported")
+        cq.exporters.export(make_t8_shaft(100), 't8_shaft.stl')  # Short for export
+        print("T8 shaft exported")
+        cq.exporters.export(make_t8_nut(), 't8_nut.stl')
+        print("T8 nut exported")
+        cq.exporters.export(make_quill_hinge(), 'quill_hinge.stl')
+        print("Quill hinge exported")
         cq.exporters.export(make_linear_rail_mount(), 'linear_rail_mount.stl')
         print("Linear rail mount exported")
-        
-        cq.exporters.export(make_carriage_adapter(), 'carriage_adapter.stl')
-        print("Carriage adapter exported")
-        
-        cq.exporters.export(make_hinge(), 'quill_hinge.stl')
-        print("Hinge exported")
-        
-        cq.exporters.export(make_t_slot_piece().translate((0, 0, 20)), 't_slot_piece.stl')
-        print("T-slot piece exported")
-        
-        # Assembly as compound for STL
         assembly = make_assembly()
         cq.exporters.export(assembly.toCompound(), 'mast_assembly.stl')
-        print("Assembly exported")
-        
+        print("Improved assembly exported")
         print("All parts exported successfully!")
         return True
-        
     except Exception as e:
         print(f"Error: {e}")
         return False
